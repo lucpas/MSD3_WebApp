1a968a28bf3c26e6c504a12395494c6f43d24953
Pascal Luckhaus <pascal_luckhaus@yahoo.de>
2019-11-28 18:01:33 +0100

    Add cors middleware to express server

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const jsonServer = require('json-server');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 app.use('/api', jsonServer.router('./data/eventData.json'));
 app.use(express.static('frontend'));
 
--- config/index_dev.js
+++ config/index_dev.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const jsonServer = require('json-server');
+const cors = require('cors');
 
 const api = express();
+
+api.use(cors());
 api.use('/api', jsonServer.router('./data/eventData.json'));
+
 api.listen(process.env.PORT || 8080);
