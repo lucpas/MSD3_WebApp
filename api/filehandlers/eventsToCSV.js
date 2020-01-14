b3d1e60e68dfbf4ade18dce637e91e3069bce168
Jakob <jakob.reindl@edu.fh-joanneum.at>
2020-01-14 10:33:53 +0100

    convertCsvUpdate

--- api/filehandlers/eventsToCSV.js
+++ api/filehandlers/eventsToCSV.js
@@ -1,10 +1,11 @@
 // Convert an incoming array of JS event objects to a CSV string
 function convertEventsArrayToCSV(events) {
 
-  let csv = "data:text/csv;charset=utf-8,";
+  let csv = '';
 
-    events.forEach(function(rowArray) {
-    let row = rowArray.join(";");
+    events.forEach(function(event) {
+    let row = [event.title, event.description, event.date, event.time, event.place, event.contact, event.institute, event.entry].join(";");
+
     csv += row + "\r\n";
     });
 
